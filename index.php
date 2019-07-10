6fc11a77972903b9d42d7b966ee8d41b0e9b89b4
alejandro <lstelf32@gmail.com>
2019-07-10 14:52:45 +0200

    primer comentario con bases,aun falta mucho

---
+++ index.php
@@ -0,0 +1,59 @@
+<!-- PHP 7 -->
+
+<?php
+
+
+?>
+
+<!-- HTML 5 -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Creador de grpos de desaroyo</title>
+
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<!-- header -->
+    <header>
+        <nav>
+     <h1>Creador de grupos de desarrollo</h1>
+        </nav>
+    </header>
+    <!-- section -->
+
+<form action="index2.php">
+    
+<label for="">Cantidad de personas</label>
+    <select>
+  <option value="1">1</option>
+  <option value="2">2</option>
+  <option value="3">3</option>
+  <option value="4">4</option>
+</select>
+
+
+<label for="">Puesto de trabajo</label>
+<select>
+  <option value="HTML5">HTML5</option>
+  <option value="CSS3">CSS3</option>
+  <option value="Javascript">Javascript</option>
+  <option value=" Json/XML"> Json/XML</option>
+  <option value="Python">Python</option>
+  <option value="Java">Java</option>
+  <option value="Sass/Less">Sass/Less</option>
+  <option value="Laravel">Laravel</option>
+
+</select>
+<input type="submit" value="Enviar">
+    <!-- footer -->
+    </form>
+    <footer>
+    
+    </footer>
+</body>
+</html>
